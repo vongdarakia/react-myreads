d84ae64b8ab84f0a6cae3b8366fe0ce677d67471
vongdarakia <vongdarakia@gmail.com>
2017-08-02 13:25:43 -0700

    feat: Add link from search page to home

--- src/Components/SearchBook.js
+++ src/Components/SearchBook.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { search, getAll } from '../BooksAPI'
 import Book from './Book';
+import { Link } from 'react-router-dom'
 
 class SearchBook extends Component {
 	constructor() {
@@ -32,7 +33,9 @@ class SearchBook extends Component {
 		return (
 			<div className="search-books">
 			  <div className="search-books-bar">
-				<a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+			  	<Link to="/">
+					<span className="close-search">Close</span>
+				</Link>
 				<div className="search-books-input-wrapper">
 				  {/*
 					NOTES: The search from BooksAPI is limited to a particular set of search terms.
