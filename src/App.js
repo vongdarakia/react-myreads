9ec3f1d8f9a90af5cb15c8515d20d185bb61e454
vongdarakia <vongdarakia@gmail.com>
2017-08-03 21:19:11 -0700

    style: Removed unnecessary code and comments

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
 import React from 'react'
-import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListBook from './Components/ListBook'
 import SearchBook from './Components/SearchBook'
 import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
-  moveTo(book, shelf) {
-	  console.log(shelf);
-	  BooksAPI.update(book, shelf);
-  }
-
-  render() {
-    return (
-		<BrowserRouter>
-	    	<div className="app">
-				<Switch>
-					<Route exact path="/" component={ListBook} />
-					<Route path="/search" moveTo={this.moveTo.bind(this)} render={(props) => {
-						return (<SearchBook {...props} moveTo={this.moveTo.bind(this)} />)
-						// return React.createElement(SearchBook, {moveTo: });
-					}}/>
-					<Redirect to="/" />
-				</Switch>
-	     	</div>
-		</BrowserRouter>
-    )
-  }
+	render() {
+		return (
+			<BrowserRouter>
+				<div className="app">
+					<Switch>
+						<Route exact path="/" component={ListBook} />
+						<Route path="/search" render={(props) => {
+							return <SearchBook {...props} />
+						}} />
+						<Redirect to="/" />
+					</Switch>
+				</div>
+			</BrowserRouter>
+		)
+ 	}
 }
 
 export default BooksApp
--- src/Components/SearchBook.js
+++ src/Components/SearchBook.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { search, getAll } from '../BooksAPI'
+import { search } from '../BooksAPI'
 import Book from './Book';
 import { Link } from 'react-router-dom'
 
@@ -10,54 +10,43 @@ class SearchBook extends Component {
 			query: "",
 			books: []
 		};
-		console.log(getAll());
 	}
+
 	onChange(e) {
-		// console.log(e)
 		let self = this;
-		console.log(self.state);
+
 		this.setState({query: e.target.value}, () => {
 			search(self.state.query, 20).then((res) => {
-				console.log(res);
 				if (res && !res.error) {
 					this.setState({books: res});
 				} else {
 					this.setState({books: []});
 				}
-				console.log(res);
 			});
 		});
 	}
+
 	render() {
-		// console.log(this.props);
-		let { moveTo } = this.props;
-		// console.log(moveTo)
 		return (
 			<div className="search-books">
-			  <div className="search-books-bar">
-			  	<Link to="/">
-					<span className="close-search">Close</span>
-				</Link>
-				<div className="search-books-input-wrapper">
-				  {/*
-					NOTES: The search from BooksAPI is limited to a particular set of search terms.
-					You can find these search terms here:
-					https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-					However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-					you don't find a specific author or title. Every search is limited by search terms.
-				  */}
-				  <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={this.onChange.bind(this)}/>
-
+				<div className="search-books-bar">
+					<Link to="/">
+						<span className="close-search">Close</span>
+					</Link>
+					<div className="search-books-input-wrapper">
+						<input type="text"
+							placeholder="Search by title or author"
+							value={this.state.query}
+							onChange={this.onChange.bind(this)} />
+					</div>
 				</div>
-			  </div>
-			  <div className="search-books-results">
-				<ol className="books-grid">
-					{this.state.books.map((book, idx) => (
-						<li key={"book-" + book.id}><Book book={book} moveTo={moveTo}/></li>
-					))}
-				</ol>
-			  </div>
+				<div className="search-books-results">
+					<ol className="books-grid">
+						{this.state.books.map((book, idx) => (
+							<li key={"book-" + book.id}><Book book={book} /></li>
+						))}
+					</ol>
+				</div>
 			</div>
 		);
 	}
